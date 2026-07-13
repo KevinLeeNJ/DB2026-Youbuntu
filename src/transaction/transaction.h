e7f55c53cc6c0bc3015b38971f04d14684453e11
Kevin Lee <zy2014lideqian@gmail.com>
2026-07-13 12:42:41 +0800

    fix(transaction): synchronize transaction state

--- src/transaction/transaction.h
+++ src/transaction/transaction.h
@@ -87,10 +87,10 @@ public:
     }
 
     inline TransactionState get_state() {
-        return state_;
+        return state_.load(std::memory_order_acquire);
     }
     inline void set_state(TransactionState state) {
-        state_ = state;
+        state_.store(state, std::memory_order_release);
     }
 
     inline lsn_t get_prev_lsn() {
@@ -194,7 +194,7 @@ public:
 
 private:
     bool txn_mode_{false};           // 用于标识当前事务为显式事务还是单条SQL语句的隐式事务
-    TransactionState state_;         // 事务状态
+    std::atomic<TransactionState> state_; // 事务状态；GC/SSI may inspect it from another thread
     IsolationLevel isolation_level_; // 事务的隔离级别
     std::thread::id thread_id_;      // 当前事务对应的线程id
     lsn_t prev_lsn_;                 // 当前事务执行的最后一条操作对应的lsn，用于系统故障恢复
